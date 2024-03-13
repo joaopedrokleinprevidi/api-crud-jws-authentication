3d6d26100568522709f79d4e9b38e80889a0cb8e
* <*>
2024-03-13 09:22:05 -0300

    att

--- backend/src/models/usersModel.js
+++ backend/src/models/usersModel.js
@@ -21,30 +21,6 @@ const getUserByLogin = async (login) => {
   return userExistence[0];
 };
 
-// const checkPasswordExistence = async (password) => {
-//   const query = "SELECT * FROM usuarios WHERE password = ?";
-//   const passwordExistence = await connection.execute(query, [password]);
-//   if (passwordExistence.length > 0) {
-//     console.log("Senha válida.");
-//     return passwordExistence;
-//   } else {
-//     console.log("Senha inválida.");
-//     return false;
-//   }
-// };
-
-// const foundUserId = async (login) => {
-//   const query = "SELECT id FROM usuarios WHERE login = ?";
-//   const userId = await connection.execute(query, [login]);
-//   if (userId.length > 0) {
-//     console.log("ID Válido.");
-//     return userId[0].id;
-//   } else {
-//     console.log("ID Inválido.");
-//     return null;
-//   }
-// };
-
 module.exports = {
   getAll,
   createUser,
